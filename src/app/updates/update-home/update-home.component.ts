3056b1a289ddbfea2bcb7d913af8fec5a94405a4
emddeveloper <emd.developer@gmail.com>
2021-04-13 12:47:57 +0530

    done

--- src/app/updates/update-home/update-home.component.ts
+++ src/app/updates/update-home/update-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SkonlineService } from '../../skonline.service';
+
 @Component({
   selector: 'app-update-home',
   templateUrl: './update-home.component.html',
@@ -15,15 +16,18 @@ export class UpdateHomeComponent implements OnInit {
     credit: '',
     timestamp: {},
   };
+  loader = false;
   getLatestRecord() {
     this.skonline.getLatestRecord().subscribe((response: any) => {
       this.latestData = response;
     });
   }
   updateRecordSubmit() {
+    this.loader = true;
     this.latestData.timestamp = new Date().getTime();
     this.skonline.updateRecord(this.latestData).subscribe((Response) => {
       console.log('submit');
+      this.loader = false;
     });
   }
   ngOnInit(): void {
